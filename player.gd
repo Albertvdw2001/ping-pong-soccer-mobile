0afbaccaa856ceb3c0d8218b7e6d0acc6c17ce0d
albert@ankerdata.com <albert@ankerdata.com>
2025-09-24 11:51:34 +0200

    Added Player and Ball scenes; Wrote script for Player touch-drag controls

---
+++ player.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var sprite = $Sprite2D
+
+var dragging = false
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		if event.is_pressed():
+			dragging = sprite.get_rect().has_point(to_local(event.position))
+		else:
+			dragging = false
+	elif event is InputEventScreenDrag and dragging:
+		global_position = event.position
